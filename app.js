140295a0522c6ec7ed3778dbdd05a4d5164b9c4e
Paul Tracy <ptracy16@gmail.com>
2017-02-22 17:50:59 +0000

    added authorization

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Campground  = require("./models/campground"),
-    seedDB      = require("./seeds"),
-    Comment     = require("./models/comment");
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    passport       = require("passport"),
+    LocalStrategy  = require("passport-local"),
+    Campground     = require("./models/campground"),
+    seedDB         = require("./seeds"),
+    User           = require("./models/user"),
+    Comment        = require("./models/comment");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Kristen is the best GF ever!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//middleware function that will add user info to routes
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+})
 
 // =============================
 // Routes for campgrounds
@@ -26,7 +48,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err)
         } else {
-            res.render("campgrounds/index", {campgrounds: allcampgrounds});
+            res.render("campgrounds/index", {campgrounds: allcampgrounds, currentUser: req.user});
         }
     });
 });
@@ -72,19 +94,18 @@ app.get("/campgrounds/:id", function(req, res){
 // Comments Routes
 // =============================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     // find campgruond by ID
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err)
         } else {
             res.render("comments/new", {campground: campground});
-    
         }
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     //lookup campground using ID
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -106,6 +127,57 @@ app.post("/campgrounds/:id/comments", function(req, res){
     // connect new comment to campground
     // redirect to back to show page of campground
 })
+
+// ================
+// AUTH ROUTES
+// ================
+
+//show register / sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// handle register / signup logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// handle login post (using middleware)
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds", 
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+//logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+})
+
+
+// middleware logic for checking if the user is logged in
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return(next())
+    }
+    res.redirect("/login");
+}
 
 // =============================
 // web application listening
