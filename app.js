37882eb83c2546fd52861e04699ef64f033acc2d
Paul Tracy <ptracy16@gmail.com>
2017-03-02 18:22:37 +0000

    update app.js

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundsRoutes   = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://paul:password@ds033477.mlab.com:33477/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
